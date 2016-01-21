e3f4ce023e2a82937af8aaecd65dca2be98f6990
ikhakoo <ikhakoo@gmail.com>
2016-01-21 14:20:55 -0500

    changing db to pg for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160114210345) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "clicks", force: :cascade do |t|
     t.integer  "url_id"
     t.string   "referral_url"
